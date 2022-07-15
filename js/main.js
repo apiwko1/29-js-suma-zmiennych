aaec0ed64af571d8496338f73ee29b6a636e0854
Adam Piwko <apiwko@gmail.com>
2022-07-15 14:57:10 +0200

    pierwsze

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 5;
+let secondNumber = 3;
+let thirdNumber = suma(firstNumber, secondNumber);
+console.log(thirdNumber);
+
+function suma(x,y){
+    let s = x + y;
+    return s;
+}
